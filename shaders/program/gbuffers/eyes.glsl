dab3088faead5c926ef290f5b1e8155b777e9489
clorece <clarencegrimaldo@gmail.com>
2026-06-30 03:10:26 -0400

    Add dedicated spider eye shader passes

---
+++ shaders/program/gbuffers/eyes.glsl
@@ -0,0 +1,58 @@
+#include "/lib/options.glsl"
+
+#ifdef VERTEX
+
+#include "/lib/util/jitter.glsl"
+
+out vec2 texCoord;
+out vec2 lightmapCoord;
+out vec3 normal;
+out vec4 color;
+
+void main() {
+    gl_Position = ftransform();
+    texCoord = gl_MultiTexCoord0.xy;
+
+    color = gl_Color;
+
+    /*
+    Normal and Lightmap Source Code By saada2006:
+    https://github.com/saada2006/MinecraftShaderProgramming
+    */
+    normal = normalize(gl_NormalMatrix * gl_Normal);
+    lightmapCoord = mat2(gl_TextureMatrix[1]) * gl_MultiTexCoord1.st;
+
+    #ifdef TAA
+        gl_Position.xy += getTaaJitter() * 2.0 * gl_Position.w / vec2(viewWidth, viewHeight);
+    #endif
+
+    gl_Position.xy = gl_Position.xy * renderScale + gl_Position.w * (renderScale - 1.0);
+}
+
+#endif
+
+#ifdef FRAGMENT
+
+/*
+Normal and Lightmap Source Code By saada2006:
+https://github.com/saada2006/MinecraftShaderProgramming
+*/
+
+in vec2 texCoord;
+in vec2 lightmapCoord;
+in vec3 normal;
+in vec4 color;
+
+void main() {
+    vec4 albedo = texture(texture, texCoord) * color;
+
+    if (albedo.a < 0.1) discard;
+
+    /* DRAWBUFFERS:0127 */
+    gl_FragData[0] = albedo;
+    gl_FragData[1] = vec4(normal * 0.5 + 0.5, 1.0);
+    gl_FragData[2] = vec4(lightmapCoord, 0.0, 1.0);
+    gl_FragData[3] = vec4(0.0);
+}
+
+#endif
---
+++ shaders/world0/gbuffers_spidereyes.fsh
@@ -0,0 +1,4 @@
+#version 430 compatibility
+#define FRAGMENT
+#include "/lib/uniforms.glsl"
+#include "/program/gbuffers/spidereyes.glsl"
